ac7edc8f2a496971d59262c29acb456b7b763b89
Toby Kad <Toby@Kad>
2023-03-19 00:48:43 +0100

    log in and out part 2

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { Image } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image, View, Text, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from './screens/HomeScreen';
 import UserScreen from './screens/UserScreen';
@@ -9,7 +11,9 @@ import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import ScheduleScreen from './screens/ScheduleScreen';
 import AuthContext from './AuthContext';
+import MainScreen from './screens/MainScreen';
 
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const App = () => {
@@ -26,72 +30,89 @@ const App = () => {
     setUserData(null);
   };
 
-  const logout = () => {
-    clearUserData();
-  };
+  const LogoutButton = () => (
+    <TouchableOpacity onPress={clearUserData}>
+      <Text style={{ color: '#333', marginRight: 10 }}>Logout</Text>
+    </TouchableOpacity>
+  );
+
+  const UserHeader = () => (
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Text style={{ color: '#333', marginRight: 10 }}>{userData?.name}</Text>
+      <LogoutButton />
+    </View>
+  );
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, userData, storeUserData, clearUserData, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, userData, storeUserData, clearUserData }}>
       <NavigationContainer>
-      <Tab.Navigator
-  screenOptions={({ route }) => ({
-    tabBarIcon: ({ focused, color, size }) => {
-      let iconPath;
-
-      if (route.name === 'SignUp') {
-        iconPath = focused
-          ? require('./assets/login.png')
-          : require('./assets/login.png');
-      }else if (route.name === 'Home') {
-        iconPath = focused
-          ? require('./assets/tavern.png')
-          : require('./assets/tavern2.png');
-      } else if (route.name === 'User') {
-        iconPath = focused
-          ? require('./assets/user.png')
-          : require('./assets/user.png');
-      } else if (route.name === 'Login') {
-        iconPath = focused
-          ? require('./assets/login.png')
-          : require('./assets/login.png');
-      } else if (route.name === 'Schedule') {
-        iconPath = focused
-          ? require('./assets/schedule.png')
-          : require('./assets/schedule.png');
-      }
-      return <Image source={iconPath} style={{ width: size, height: size }} />;
-    },
-  })}
-  tabBarOptions={{
-    activeTintColor: '#000',
-    inactiveTintColor: '#ccc',
-    style: {
-      backgroundColor: '#fff',
-    },
-  }}
->
-  <Tab.Screen name="Home" component={HomeScreen} />
-  <Tab.Screen name="User" component={UserScreen} />
-  {!isLoggedIn && <Tab.Screen name="Login" component={LoginScreen} />}
-  {!isLoggedIn && <Tab.Screen name="SignUp" component={SignUpScreen} />}
-  {isLoggedIn && (
-  <Tab.Screen
-    name="Logout"
-    component={LoginScreen}
-    listeners={({ navigation }) => ({
-      tabPress: (e) => {
-        e.preventDefault();
-        logout();
-        navigation.navigate('Login');
-      },
-    })}
-  />
-)}
-
-  <Tab.Screen name="Schedule" component={ScheduleScreen} />
-</Tab.Navigator>
-
-    </NavigationContainer>
+        {isLoggedIn ? (
+          <Tab.Navigator>
+            <Tab.Screen 
+              name="Home" 
+              component={HomeScreen}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Ionicons
+                    name={focused ? 'home-sharp' : 'home-outline'}
+                    size={size}
+                    color={color}
+                  />
+                )
+              }}
+            />
+            <Tab.Screen 
+              name="Schedule" 
+              component={ScheduleScreen}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Ionicons
+                    name={focused ? 'calendar-sharp' : 'calendar-outline'}
+                    size={size}
+                    color={color}
+                  />
+                )
+              }}
+            />
+            <Tab.Screen 
+              name="Logout" 
+              component={LoginScreen}
+              listeners={({ navigation }) => ({
+                tabPress: (e) => {
+                  // Prevent default action of navigating to tab
+                  e.preventDefault();
+                  clearUserData();
+                  navigation.navigate('Login');
+                },
+              })}
+              options={{
+                tabBarIcon: ({ focused, color, size }) => (
+                  <Ionicons
+                    name={focused ? 'log-out-sharp' : 'log-out-outline'}
+                    size={size}
+                    color={color}
+                  />
+                )
+              }}
+            />
+          </Tab.Navigator>
+        ) : (
+          <Stack.Navigator
+            screenOptions={{
+              headerTintColor: '#fff',
+              headerStyle: {
+                backgroundColor: '#333',
+              },
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          >
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
     </AuthContext.Provider>
   );
 };
